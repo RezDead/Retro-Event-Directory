7930647bdf06c03130a6ba05ac21abc6a60b54c7
RezDead <razorjcgames@gmail.com>
2026-06-17 15:59:39 -0700

    Initial Commit

---
+++ projects.js
@@ -0,0 +1,67 @@
+const projects = [
+  {
+    id: "sample-project",
+    title: "Sample Project",
+    location: "Chico, CA",
+    people: ["Name One", "Name Two"],
+    image: "./images/sample.jpg",
+
+    year: "2026",
+    category: "Interactive Installation",
+
+    summary: "A short one-paragraph overview of the project.",
+
+    description: [
+      "Longer paragraph one about the project.",
+      "Longer paragraph two about the process, goals, or outcome."
+    ],
+
+    details: [
+      {
+        label: "Tools",
+        value: "HTML, CSS, JavaScript, Arduino"
+      },
+      {
+        label: "Materials",
+        value: "CRT monitor, found electronics, acrylic"
+      },
+    ],
+
+    gallery: [
+      "./images/sample-1.jpg",
+      "./images/sample-2.jpg",
+      "./images/sample-3.jpg"
+    ]
+  },
+  {
+    id: "event-directory",
+    title: "Event Directory",
+    location: "Chico, CA",
+    people: ["Julian Kroeger-Miller"],
+    image: "./images/Directory/Directory.jpg",
+
+    year: "2026",
+    category: "Interactive Directory",
+
+    summary: "A website serving as the event directory and project hub.",
+
+    description: [
+      "This website actually is a project! I didn't necessarily know what project I would want to create so I decided to become an amplifier for all of the other projects people would create.",
+      "In total I created 5 fully interactive elements and 2 page templates that allow you to view people project and have a little bit of fun.",
+      "I deeply hope you enjoy this event! We all worked really hard on these projects."
+    ],
+
+    details: [
+      {
+        label: "Tools",
+        value: "HTML, CSS, JavaScript, ChatGPT"
+      },
+    ],
+
+    gallery: [
+      "./images/Directory/Project.jpg",
+      "./images/Directory/Stickers.jpg",
+      "./images/Directory/BSOD.jpg"
+    ]
+  },
+];
